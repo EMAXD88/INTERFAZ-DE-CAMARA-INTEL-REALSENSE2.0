22d09d52b88e5bc6dd091ef2339a5a5f9417250d
EMAXD88 <emmanuelbalbuena164@gmail.com>
2023-09-25 21:16:34 -0600

    dos
    nuevo cambio

--- InterfazCamara2.0.py
+++ InterfazCamara2.0.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import messagebox, simpledialog
+from tkinter import messagebox, ttk
+from tkcalendar import Calendar  # Importamos el widget de calendario
 import cv2
 import time
 import os
@@ -14,6 +15,17 @@ devices = ctx.query_devices()
 for dev in devices:
     dev.hardware_reset()
 print("reset done")
+
+# Variables globales para la cámara y su estado
+pipeline = None
+align = None
+camera_running = False
+image_counter = 1  # Contador de imágenes
+image_preview = None  # Variable para almacenar la vista previa de la imagen
+
+# Variables globales para las imágenes
+color_image = None
+depth_image = None
 
 # Función para iniciar la cámara RealSense
 def start_camera():
@@ -33,82 +45,115 @@ def start_camera():
     depth_sensor.set_option(rs.option.enable_auto_white_balance, False)
     depth_sensor.set_option(rs.option.visual_preset, rs.option.visual_preset_custom)
 
+# Función para mostrar la vista previa de la imagen en una ventana emergente
+def show_image_preview(color_img, depth_img):
+    global image_preview, color_image, depth_image
+    color_image = color_img
+    depth_image = depth_img
+
+    image_preview = tk.Toplevel(root)
+    image_preview.title("Vista Previa de la Imagen")
+
+    # Muestra la imagen en color
+    color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+    color_photo = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(color_image, (600, 400))))
+    color_label = tk.Label(image_preview, image=color_photo)
+    color_label.image = color_photo
+    color_label.pack(side=tk.LEFT)
+
+    # Muestra la imagen de profundidad
+    depth_image_rectified = cv2.normalize(depth_image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    depth_photo = ImageTk.PhotoImage(image=Image.fromarray(cv2.resize(depth_image_rectified*4, (600, 400))))
+    depth_label = tk.Label(image_preview, image=depth_photo)
+    depth_label.image = depth_photo
+    depth_label.pack(side=tk.LEFT)
+
+    # Crear entrada para el nombre de la imagen
+    name_label = tk.Label(image_preview, text="Nombre de la Imagen:")
+    name_label.pack()
+    name_entry = tk.Entry(image_preview)
+    name_entry.pack()
+
+    # Crear widget de calendario para seleccionar la fecha
+    date_label = tk.Label(image_preview, text="Fecha (opcional):")
+    date_label.pack()
+    date_calendar = Calendar(image_preview, date_pattern="yyyy-mm-dd")
+    date_calendar.pack()
+
+    # Botón para guardar la imagen
+    save_button = tk.Button(image_preview, text="Proceder a Guardar", command=lambda: save_image(name_entry.get(), date_calendar.get_date()))
+    save_button.pack()
+
 # Función para tomar una foto
 def take_photo():
-    global camera_running, image_counter, previous_images
+    global camera_running, image_counter
     if not camera_running:
         messagebox.showerror("ERROR", "LA CAMARA NO SE HA INICIADO CORRECTAMENTE!")
         return
 
+    # Captura las imágenes RGB y de profundidad
     frames = pipeline.wait_for_frames()
     aligned_frames = align.process(frames)
     aligned_depth_frame = aligned_frames.get_depth_frame()
     color_frame = aligned_frames.get_color_frame()
+    
     if not aligned_depth_frame or not color_frame:
         messagebox.showerror("ERROR", "FALLO EN LA CAPTURA!")
         return
 
+    # Convierte las imágenes a formato numpy
     depth_image = np.asanyarray(aligned_depth_frame.get_data())
     color_image = np.asanyarray(color_frame.get_data())
 
-    # Obtener la fecha actual en el formato YYYYMMDD
+    # Muestra la vista previa de la imagen
+    show_image_preview(color_image, depth_image)
+
+# Función para guardar la imagen con el nombre y la fecha proporcionados
+def save_image(name, date_str):
+    global image_counter, image_preview
+    if not name:
+        messagebox.showerror("ERROR", "Debe ingresar un nombre para la imagen.")
+        return
+    elif not date_str:
+        messagebox.showerror("ERROR", "Debe seleccionar una fecha para la imagen.")
+        return
+
+    # Convierte la cadena de fecha a un objeto datetime
+    date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+
+    # Cierra la ventana de vista previa
+    image_preview.destroy()
+
+    # Crear subcarpeta para el día actual si no existe
     current_date = datetime.datetime.now().strftime("%Y%m%d")
     folder_name = os.path.join("CapturasIntelRealSense", current_date)
     os.makedirs(folder_name, exist_ok=True)
 
-    # Crear subcarpetas para las imágenes RGB y de profundidad
+    # Crear subcarpetas para las imágenes RGB, de profundidad y .npy
     rgb_folder = os.path.join(folder_name, "RGB")
     depth_folder = os.path.join(folder_name, "Profundidad")
-    os.makedirs(rgb_folder, exist_ok=True)
-    os.makedirs(depth_folder, exist_ok=True)
-
-    # Crear subcarpetas para las imágenes RGB y de profundidad en formato npy
     npy_rgb_folder = os.path.join(folder_name, "RGB_npy")
     npy_depth_folder = os.path.join(folder_name, "Profundidad_npy")
+    os.makedirs(rgb_folder, exist_ok=True)
+    os.makedirs(depth_folder, exist_ok=True)
     os.makedirs(npy_rgb_folder, exist_ok=True)
     os.makedirs(npy_depth_folder, exist_ok=True)
 
-    # Pedir al usuario que introduzca un nombre para la foto
-    photo_name = simpledialog.askstring("Nombre de la foto", "Introduce un nombre para la foto:")
-
-    if not photo_name:
-        messagebox.showinfo("FOTO NO GUARDADA", "La foto no ha sido guardada debido a la falta de nombre.")
-        return
-
-    current_time = time.strftime("%H%M%S")
-    rgb_image_path = os.path.join(rgb_folder, f"{photo_name}_{current_time}_rgb_{image_counter}.png")
-    depth_image_path = os.path.join(depth_folder, f"{photo_name}_{current_time}_profundidad_{image_counter}.png")
-    npy_rgb_image_path = os.path.join(npy_rgb_folder, f"{photo_name}_{current_time}_rgb_{image_counter}.npy")
-    npy_depth_image_path = os.path.join(npy_depth_folder, f"{photo_name}_{current_time}_profundidad_{image_counter}.npy")
-
+    # Guarda las imágenes con el nombre y la fecha proporcionados
+    image_counter += 1
+    image_name = name + "_" + date.strftime("%Y%m%d")
+    
+    rgb_image_path = os.path.join(rgb_folder, f"{image_name}_rgb.png")
+    depth_image_path = os.path.join(depth_folder, f"{image_name}_profundidad.png")
+    npy_rgb_image_path = os.path.join(npy_rgb_folder, f"{image_name}_rgb.npy")
+    npy_depth_image_path = os.path.join(npy_depth_folder, f"{image_name}_profundidad.npy")
+    
     cv2.imwrite(rgb_image_path, color_image)
     cv2.imwrite(depth_image_path, depth_image)
-
     np.save(npy_rgb_image_path, color_image)
     np.save(npy_depth_image_path, depth_image)
-
-    # Agregar la imagen previa a la lista y mostrarla en una ventana flotante
-    previous_images.append((photo_name, rgb_image_path))
-    show_previous_images()
-
-    messagebox.showinfo("FOTO TOMADA", "LA FOTO HA SIDO TOMADA Y GUARDADA!")
-
-# Función para mostrar imágenes previas en una ventana flotante
-def show_previous_images():
-    global previous_images
-    if not previous_images:
-        return
-
-    prev_image_window = tk.Toplevel(root)
-    prev_image_window.title("Imágenes Previas")
-
-    for name, path in previous_images:
-        image = Image.open(path)
-        photo = ImageTk.PhotoImage(image)
-        label = tk.Label(prev_image_window, text=name)
-        label.pack()
-        label.img = photo
-        label.config(image=photo)
+    
+    messagebox.showinfo("FOTO GUARDADA", f"Las imágenes se guardaron como:\n{rgb_image_path}\n{depth_image_path}\n{npy_rgb_image_path}\n{npy_depth_image_path}")
 
 # Función para cerrar la aplicación y detener la cámara
 def close_app():
@@ -139,16 +184,12 @@ def update_camera_frame():
             label_depth.config(image=depth_photo)
             label_depth.image = depth_photo
 
+    root.after(60, update_camera_frame)  # Ajustamos el retraso a 30 ms para mejorar el rendimiento
+
 # Creación de la ventana principal de la interfaz
 root = tk.Tk()
 root.title("INTERFAZ DE LA CAMARA")
 root.attributes('-fullscreen', True)
-
-# Variables globales para la cámara y su estado
-pipeline = None
-align = None
-camera_running = False
-image_counter = 0
 
 # Creación del marco para las vistas de la cámara
 frame_camera = tk.Frame(root)
